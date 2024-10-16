4bebd23be8379f984b84f2ee4a8b0ff6671c8169
Zabir Raihan <zabirraihan@icloud.com>
2024-10-16 15:15:51 +0800

    Pass props as env valeus rather than parameters

---
+++ Sources/MarkdownWebView/EnvironmentValues.swift
@@ -0,0 +1,65 @@
+//
+//  MarkdownFontSizeKey.swift
+//  markdown-webview
+//
+//  Created by Zabir Raihan on 16/10/2024.
+//
+
+
+import SwiftUI
+
+private struct MarkdownFontSizeKey: EnvironmentKey {
+    static let defaultValue: CGFloat = 13
+}
+
+private struct MarkdownThemeKey: EnvironmentKey {
+    static let defaultValue: MarkdownTheme = .atom
+}
+
+private struct MarkdownHighlightStringKey: EnvironmentKey {
+    static let defaultValue: String? = nil
+}
+
+private struct MarkdownBaseURLKey: EnvironmentKey {
+    static let defaultValue: String = "Web Content"
+}
+
+extension EnvironmentValues {
+    var markdownFontSize: CGFloat {
+        get { self[MarkdownFontSizeKey.self] }
+        set { self[MarkdownFontSizeKey.self] = newValue }
+    }
+    
+    var markdownTheme: MarkdownTheme {
+        get { self[MarkdownThemeKey.self] }
+        set { self[MarkdownThemeKey.self] = newValue }
+    }
+    
+    var markdownHighlightString: String? {
+        get { self[MarkdownHighlightStringKey.self] }
+        set { self[MarkdownHighlightStringKey.self] = newValue }
+    }
+    
+    var markdownBaseURL: String {
+        get { self[MarkdownBaseURLKey.self] }
+        set { self[MarkdownBaseURLKey.self] = newValue }
+    }
+}
+
+extension View {
+    public func markdownFontSize(_ size: CGFloat) -> some View {
+        environment(\.markdownFontSize, size)
+    }
+    
+    public func markdownTheme(_ theme: MarkdownTheme) -> some View {
+        environment(\.markdownTheme, theme)
+    }
+    
+    public func markdownHighlightString(_ string: String?) -> some View {
+        environment(\.markdownHighlightString, string)
+    }
+    
+    public func markdownBaseURL(_ url: String) -> some View {
+        environment(\.markdownBaseURL, url)
+    }
+}
--- Sources/MarkdownWebView/MarkdownWebView.swift
+++ Sources/MarkdownWebView/MarkdownWebView.swift
@@ -10,48 +10,38 @@ import WebKit
 @available(macOS 11.0, iOS 14.0, *)
 public struct MarkdownWebView: PlatformViewRepresentable {
     var markdownContent: String
-    let customStylesheet: MarkdownTheme
-    let highlightString: String? // Pass if want to highlight substrings
-    let baseURL: String // Shows up on activity monitor
-    let fontSize: CGFloat
+    
+    @Environment(\.markdownFontSize) var fontSize
+    @Environment(\.markdownTheme) var customStylesheet
+    @Environment(\.markdownHighlightString) var highlightString
+    @Environment(\.markdownBaseURL) var baseURL
 
-    public init(
-        _ markdownContent: String,
-        baseURL: String = "Web Content",
-        highlightString: String? = nil,
-        customStylesheet: MarkdownTheme = .atom,
-        fontSize: CGFloat = 13
-    ) {
+    public init(_ markdownContent: String) {
         self.markdownContent = markdownContent
-        self.customStylesheet = customStylesheet
-        self.highlightString = highlightString
-        self.baseURL = baseURL
-        self.fontSize = fontSize
     }
 
     public func makeCoordinator() -> Coordinator { .init(parent: self) }
 
     #if os(macOS)
-        public func makeNSView(context: Context) -> CustomWebView { context.coordinator.platformView }
+    public func makeNSView(context: Context) -> CustomWebView { context.coordinator.platformView }
     #else
-        public func makeUIView(context: Context) -> CustomWebView { context.coordinator.platformView }
+    public func makeUIView(context: Context) -> CustomWebView { context.coordinator.platformView }
     #endif
 
     public func updatePlatformView(_ platformView: CustomWebView, context _: Context) {
         guard !platformView.isLoading else { return }
         
-        // Load the new stylesheet
 //        if let customStylesheetFileURL = Bundle.module.url(forResource: self.customStylesheet.fileName, withExtension: ""),
 //           let customStylesheet = try? String(contentsOf: customStylesheetFileURL) {
 //            platformView.updateStylesheet(customStylesheet)
 //        }
-//        
+        
         platformView.updateMarkdownContent(markdownContent, highlightString: highlightString, fontSize: fontSize)
     }
 
     #if os(macOS)
-        public func updateNSView(_ nsView: CustomWebView, context: Context) { updatePlatformView(nsView, context: context) }
+    public func updateNSView(_ nsView: CustomWebView, context: Context) { updatePlatformView(nsView, context: context) }
     #else
-        public func updateUIView(_ uiView: CustomWebView, context: Context) { updatePlatformView(uiView, context: context) }
+    public func updateUIView(_ uiView: CustomWebView, context: Context) { updatePlatformView(uiView, context: context) }
     #endif
 }
